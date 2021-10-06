f12f284ec6d409ce1bb48dbbdc60ae0811bd7054
Corey Lanier <iam@coreylanier.com>
2021-10-05 23:54:42 -0400

    add items to shopping list

---
+++ scraper.js
@@ -0,0 +1,36 @@
+const rp = require("request-promise]");
+
+const items = [
+  {
+    url: "https://focusattack.com/samducksa-sdl-301-dx-qr-quick-release-joystick-lever/",
+    qty: 1,
+  },
+  {
+    url: "https://focusattack.com/samducksa-sdl-301-dx-s-joystick-lever-2018-model/",
+    qty: 1,
+  },
+  {
+    url: "https://focusattack.com/crown-samducksa-sdb-202c-mx-30mm-translucent-screwbutton-purple/",
+    qty: 2,
+  },
+  {
+    url: "https://focusattack.com/crown-samducksa-sdb-202c-mx-30mm-translucent-screwbutton-red/",
+    qty: 6,
+  },
+  {
+    url: "https://focusattack.com/crown-samducksa-sdb-202-mx-30mm-screwbutton-violet/",
+    qty: 4,
+  },
+  {
+    url: "https://focusattack.com/crown-samducksa-sdb-202-mx-30mm-screwbutton-pink/",
+    qty: 4,
+  },
+  {
+    url: "https://focusattack.com/brook-ps3-ps4-fighting-board-ps4-ps3-pc/",
+    qty: 2,
+  },
+  {
+    url: "https://focusattack.com/kori-hollow-battop-pink/",
+    qty: 1,
+  },
+];
